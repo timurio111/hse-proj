610e106ce0493a4a390d18b92a3eee5727f141f2
MeganJard <57149105+MeganJard@users.noreply.github.com>
2023-02-25 00:41:57 +0300

    Он бегает

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from button import Button
 pygame.init()
 monitor = pygame.display.get_desktop_sizes()[0]
 
-WIDTH, HEIGHT = [0.9 * i // 1 for i in monitor]
+WIDTH, HEIGHT = monitor
 MAX_FPS = 60
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 
 EXIT_GAME_EVENT = pygame.event.Event(pygame.QUIT)
 START_GAME_EVENT = pygame.event.Event(pygame.USEREVENT + 1)
@@ -20,13 +20,16 @@ def load_background(image_name: str) -> pygame.Surface:
     image = pygame.transform.scale(pygame.image.load(path), (WIDTH, HEIGHT))
     return image.convert()
 
-def load_image(image_name, height_to_monitor_size): #height_to_monitor_size обозначает, какая высота дожлна быть у элемента относительно высоты окна игры
+
+def load_image(image_name,
+               height_to_monitor_size):  # height_to_monitor_size обозначает, какая высота дожлна быть у элемента относительно высоты окна игры
     image_raw = pygame.image.load(image_name).convert_alpha()
     Koeff = (HEIGHT * height_to_monitor_size * (1 / image_raw.get_height()))
     width = image_raw.get_width() * Koeff // 1
     height = image_raw.get_height() * Koeff // 1
 
     return pygame.transform.scale(image_raw, [width, height])
+
 
 class Menu:
     def __init__(self):
@@ -56,51 +59,86 @@ class Player(pygame.sprite.Sprite):
         self.vx = 0
         self.vy = 0
         self.counter = 0
-        self.animations = dict()                 #Словарь c анимациями
-        self.animations_counter = 0              #Анимационный счетчик кадра
-        self.frame_number = 1                    #Связанный с animations.counter счетчик. Говорит, какой кадр нужно показывать
-        self.all_frames = 4                      #Сколько кадров для текущего статуса
-        self.frames_change_rate = 15             #Через сколько итераций менять кадр
-        self.status = 'Idle'                     #Текущий статус. Должен совпадать с названием папки с анимацией!!!
-
-
+        self.animations = dict()  # Словарь c анимациями
+        self.animations_counter = 0  # Анимационный счетчик кадра
+        self.frame_number = 0  # Связанный с animations.counter счетчик. Говорит, какой кадр нужно показывать
+        self.all_frames = 4  # Сколько кадров для текущего статуса
+        self.frames_change_rate = 10  # Через сколько итераций менять кадр
+        self.status = 'Idle'  # Текущий статус. Должен совпадать с названием папки с анимацией!!!
+        self.direction = 'right'
         for i in os.listdir(dir_for_sprites):
-            self.animations[i] = dict()
-            for j in os.listdir(dir_for_sprites + r'/' + i):
-                self.animations[i][int(j.split('.')[0]) - 1] = load_image(dir_for_sprites + '/' + i + '/' + j, 0.2)
-        self.rect = self.animations['Idle'][0].get_rect()
+            self.animations[i + 'right'] = dict()
+            self.animations[i + 'left'] = dict()
+            for j in os.listdir(dir_for_sprites + '/' + i):
+                self.animations[i + 'right'][int(j.split('.')[0]) - 1] = load_image(dir_for_sprites + '/' + i + '/' + j, 0.2)
+                self.animations[i + 'left'][int(j.split('.')[0]) - 1] = pygame.transform.flip(load_image(dir_for_sprites + '/' + i + '/' + j,
+                                                                                  0.2), flip_x=True, flip_y=False)
+        self.rect = self.animations['Idle' + 'right'][0].get_rect()
         print(self.animations)
 
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
 
+    def status_to_run(self):
+        if self.status != 'Run':
+            self.status = 'Run'
+            self.frame_number = 1
+            self.frames_change_rate = 6
+            self.all_frames = 8
+            self.animations_counter = 0
+
+    def status_to_jump(self):
+        if self.status != 'jump':
+            self.status = 'jump'
+            self.frame_number = 3
+            self.frames_change_rate = 4
+            self.all_frames = 11
+            self.animations_counter = 0
+
+    def status_to_idle(self):
+        if self.status != 'Idle':
+            self.status = 'Idle'
+            self.frames_change_rate = 12
+            self.all_frames = 4
+            self.animations_counter = 0
+            self.frame_number = 0
+
     def move_left(self):
+        self.direction = 'left'
         self.vx = -self.v
+        self.status_to_run()
 
     def move_right(self):
+        self.direction = 'right'
         self.vx = self.v
+        self.status_to_run()
 
     def jump(self):
         if self.counter == 0:
             self.vy = -self.v
+            self.status_to_jump()
 
     def loop(self, fps):
-        self.vy += min(1, self.counter / fps * 20)
-        if self.rect.y + self.rect.height >= HEIGHT and self.vy > 0:
+        self.vy += min(3, self.counter / fps * 10)
+        if self.rect.y + self.rect.height > HEIGHT and self.vy > 0:
             self.vy = 0
+            self.rect.y = HEIGHT - self.rect.height
             self.counter = 0
+
+
         else:
             self.counter += 1
         self.move(self.vx, self.vy)
 
     def draw(self, screen):
         self.animations_counter += 1
+
         if self.animations_counter == self.frames_change_rate:
             self.frame_number = (self.frame_number + 1) % self.all_frames
             self.animations_counter = 0
-      #  pygame.draw.rect(screen, (255, 255, 255), self.rect)
-        screen.blit(self.animations[self.status][self.frame_number], self.rect)
+
+        screen.blit(self.animations[self.status + self.direction][self.frame_number], self.rect)
 
 
 class Game:
@@ -117,7 +155,8 @@ class Game:
 
     def input_handle(self):
         keys = pygame.key.get_pressed()
-
+        if self.player.vx == 0 and self.player.vy == 0:
+            self.player.status_to_idle()
         self.player.vx = 0
         if keys[pygame.K_a]:
             self.player.move_left()
@@ -156,6 +195,8 @@ def main(screen):
             game.input_handle()
             game.draw(screen)
         pygame.display.update()
+        print(game.player.direction)
+
     pygame.quit()
     quit(0)
 
