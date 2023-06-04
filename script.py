d3297b402c28aaa748271f95ea5048f8222ad328
Askar <shayahmetovaskar@yandex.ru>
2023-06-04 14:38:49 +0300

    socket reuse address
    Signed-off-by: Askar <shayahmetovaskar@yandex.ru>

--- script.py
+++ script.py
@@ -9,6 +9,7 @@ HOST = '127.0.0.1'
 PORT = 5557
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 s.listen(1)
 print('waiting')
